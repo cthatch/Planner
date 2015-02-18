617c1aa5c9dcb84b29adcd69a6fd3357fd3c2b4e
STEPHEN THATCHER <cthatch07@tamu.edu>
2015-02-18 08:40:06 -0600

    Initial commit from local git

---
+++ Planner/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  Planner
+//
+//  Created by Cole Thatcher on 2/18/15.
+//  Copyright (c) 2015 Cole Thatcher. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
---
+++ Planner/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  Planner
+//
+//  Created by Cole Thatcher on 2/18/15.
+//  Copyright (c) 2015 Cole Thatcher. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
